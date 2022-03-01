055d4fa082052fd55b5dd96c2591c6ef944ef94e
Sam Parmenter <samparmenter@gmail.com>
2022-03-01 15:34:07 +0000

    Update project to latest deps etc

--- mix.exs
+++ mix.exs
@@ -1,20 +1,23 @@
 defmodule ExW3.MixProject do
   use Mix.Project
 
-  @source_url "https://github.com/hswick/exw3"
-  @version "0.6.1"
+  @source_url "https://github.com/samparmenter/exw3"
+  @version "0.6.2"
 
   def project do
     [
       app: :exw3,
       version: @version,
-      elixir: "~> 1.10",
+      elixir: "~> 1.12",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
       deps: deps(),
+      source_url: @source_url,
       docs: docs(),
       package: package(),
-      name: "exw3",
       dialyzer: [
         remove_defaults: [:unknown]
       ],
@@ -29,12 +32,16 @@ defmodule ExW3.MixProject do
     [applications: [:logger, :ex_abi, :ethereumex]]
   end
 
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :docs, runtime: false},
-      {:ethereumex, "~> 0.7.0"},
-      {:ex_keccak, "~> 0.2"},
-      {:ex_abi, "~> 0.5.4"},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
+      {:ethereumex, "~> 0.9"},
+      {:ex_keccak, "~> 0.4.0"},
+      {:ex_abi, "~> 0.5.11"},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:jason, "~> 1.2"}
     ]
@@ -53,12 +60,12 @@ defmodule ExW3.MixProject do
 
   defp docs do
     [
+      main: "readme", # The main page in the docs
+      logo: "assets/ex3w_logo.jpg",
       extras: [
         LICENSE: [title: "License"],
         "README.md": [title: "Overview"]
       ],
-      main: "readme",
-      assets: "assets",
       source_url: @source_url,
       source_ref: "v#{@version}",
       formatters: ["html"]
